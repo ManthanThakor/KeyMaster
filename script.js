7cc2a4e70bd4604e5104b45dc68d6b827c34e997
ManthanThakor <thakormanthan849@gmail.com>
2024-12-05 14:51:45 +0530

    change

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Open modal when Ctrl + / is pressed
-document.addEventListener("keydown", function (event) {
-  if (event.ctrlKey && event.key === "/") {
-    document.getElementById("shortcutModal").style.display = "flex";
-  }
-});
-
-// Close the modal when "Close" button is clicked
-document.getElementById("closeModal").addEventListener("click", function () {
-  document.getElementById("shortcutModal").style.display = "none";
+// Screenshot carousel
+$(".screenshot-carousel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 1000,
+  loop: true,
+  dots: true,
+  items: 1,
 });
